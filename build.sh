7f5bf325b36b0899946170baf78fc6614cf434ac
xuzhichaogit <zhichaogit@163.com>
2018-05-18 05:07:14 +0000

    modify the test case

--- build.sh
+++ build.sh
@@ -17,6 +17,8 @@ rm -rf KafkaCDC
 
 #java -cp bin:bin/*:libs/* KafkaCDC -p 1 -b 192.168.0.71:9092 -d 192.168.0.71 -g 1 -s SEABASE --table emps -t test -f normal --full
 #java -cp bin:bin/*:libs/* KafkaCDC -p 1 -b 192.168.0.71:9092 -d 192.168.0.71 -g 1 -s SEABASE --table TF_F_ACCOUNT -t test1 -f unicom --full
-java -cp bin:bin/*:libs/* KafkaCDC -p 1 -b 192.168.0.71:9092 -d 192.168.0.71 -g 1 -s SEABASE --table TF_F_USER_ITEM -t test2 -f unicom --full --sto 5 --interval 2
+#java -cp bin:bin/*:libs/* KafkaCDC -p 1 -b 192.168.0.71:9092 -d 192.168.0.71 -g 1 -s SEABASE --table TF_F_USER_ITEM -t test2 -f unicom --full --sto 5 --interval 2
+java -cp bin:bin/*:libs/* KafkaCDC -p 1 -b 192.168.0.71:9092 -d 192.168.0.71 -g 1 -s SEABASE --table TS_B_BILL -t test4 -f unicom --full --sto 3 --interval 2
+java -cp bin:bin/*:libs/* KafkaCDC -p 1 -b 192.168.0.71:9092 -d 192.168.0.71 -g 1 -s SEABASE -t test5 -f unicom --full --sto 3 --interval 2
 #java -cp bin:bin/*:libs/* KafkaCDC -p 2 -b 192.168.0.71:9092 -d 192.168.0.71 -g 1 -s SEABASE --table TF_F_USER_ITEM -t test -f unicom --full
 #java -cp bin:bin/*:libs/* KafkaCDC -p 1 -b 192.168.0.71:9092 -d 192.168.0.71 -g 1 -s SEABASE --table TD_B_PARTY_PRODUCT -t test3 -f unicom --full
